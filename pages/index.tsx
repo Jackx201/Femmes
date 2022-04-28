4c467c0f7cbdd517c2b5c638471086c2cba62c9d
zdominguez <zahuldmz@live.com>
2022-04-28 13:11:58 -0500

    missing section added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,19 +5,41 @@ import React from "react";
 
 const Home = () => {
   const data = require("./api/data.json");
+  const missing = require("./api/missing.json")
   return (
     <>
       <HeaderComponent></HeaderComponent>
       <div className={styles.container}>
+        <h1>Artículos</h1>
+      </div>
+      <div className={styles.container}>
         <div className="grid my-3">
           {data.map((data?: any) => (
-            <div className="col">
+            <div className="col" key={data.id}>
               <CardComponent
                 id={data.id}
                 title={data.title}
                 subtitle={data.subtitle}
                 body={data.body}
                 imgsrc={data.img}
+              ></CardComponent>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.container}>
+        <h1>Ayudanos a Encontrarlas</h1>
+      </div>
+      <div className={styles.container}>
+        <div className="grid my-3">
+          {missing.map((missing?: any) => (
+            <div className="col" key={missing.id}>
+              <CardComponent
+                id={missing.id}
+                title={missing.title}
+                subtitle={missing.subtitle}
+                body={missing.body}
+                imgsrc={missing.img}
               ></CardComponent>
             </div>
           ))}
